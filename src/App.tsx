794e12cc162cb52b167dc2bd3a1198dd27ad5c08
stark0908 <jaiswalmjo9@yahoo.com>
2025-04-13 00:36:20 +0530

    error resolved in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -7,9 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Dashboard from "./pages/Dashboard";
 import Emergency from "./pages/Emergency";
-import Report from "./pages/report";
+import Report from "./pages/Report";
 import User from "./pages/User";
-import SOS from "./pages/sos";
+import SOS from "./pages/SOS";
 import Map from "./components/Map";
 
 const queryClient = new QueryClient();
@@ -21,12 +20,12 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<User/>}/>
-          <Route path="/index" element={<Index/>} />
+          <Route path="/" element={<User />} />
+          <Route path="/index" element={<Index />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/report" element={<Report/>}/>
-          <Route path="/sos" element={<SOS/>}/>
-          <Route path="/map" element={<Map/>}/>
+          <Route path="/report" element={<Report />} />
+          <Route path="/sos" element={<SOS />} />
+          <Route path="/map" element={<Map />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="/Emergency" element={<Emergency />} />
         </Routes>
